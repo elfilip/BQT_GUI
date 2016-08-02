50f335d6b552daef7508410bec94e1621ad68509
Juraj Duráni <jdurani@redhat.com>
2016-08-02 09:38:40 +0200

    XmlTextPane - added undo/redo action

--- src/main/java/qe/gui/highlight/XMLTextPane.java
+++ src/main/java/qe/gui/highlight/XMLTextPane.java
@@ -1,6 +1,14 @@
 package qe.gui.highlight;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
+import javax.swing.undo.UndoManager;
 
 /**
  * 
@@ -14,5 +22,26 @@ public class XMLTextPane extends JTextPane {
 	public XMLTextPane() {
 		setEditorKitForContentType("text/xml", new XMLEditorKit());
 		setContentType("text/xml");
+        final UndoManager man = new UndoManager();
+        man.setLimit(500);
+        getDocument().addUndoableEditListener(man);
+        getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK), "undo");
+        getActionMap().put("undo", new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(man.canUndo()){
+                    man.undo();
+                }
+            }
+        });
+        getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_DOWN_MASK), "redo");
+        getActionMap().put("redo", new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(man.canRedo()){
+                    man.redo();
+                }
+            }
+        });
 	}
 }
